04a4317207e43159a94b5147834e2ebf6fb33784
usernamealexgit config --global user.email shpilevoy29@gmail.comgit commit -mcomment <shpilevoy29@gmai.com>
2021-03-24 08:13:24 +0200

    final

--- app.py
+++ app.py
@@ -12,38 +12,37 @@ from models import Candidate
 
 from models import Vacancy
 
-
 if __name__ == '__main__':
+    igor = Programmer('Igor', 1000, 'igor@gmail.com', 'programmer', ['JS', 'Python', 'Delphi'])
 
-        igor = Programmer('Igor', 1000, 'igor@gmail.com', 'programmer', ['JS', 'Python', 'Delphi'])
+    sasha = Programmer('Sasha', 130, 'sasha@gmai.com', 'programmer', ['Python'])
 
-        sasha = Programmer('Sasha', 130, 'sasha@gmai.com', 'programmer', ['Python'])
+    curt = Recruiter('Cobain', 180, 'ineedagun@bang.us', 'recruiter')
 
-        curt = Recruiter('Cobain', 180, 'ineedagun@bang.us', 'recruiter')
-
-        john = Candidate('John Fitzgerald Kennedy', 'oneshot@president.us', ["Python", 'JS'], ['Pylon', 'JS'],
+    john = Candidate('John Fitzgerald Kennedy', 'oneshot@president.us', ["Python", 'JS'], ['Pylon', 'JS'],
                      ['Python -middle', 'JS-junior'])
 
-        java = Vacancy('Java', ['Java game developer'], ['Java - middle', 'Python - senior'])
+    java = Vacancy('Java', ['Java game developer'], ['Java - middle', 'Python - senior'])
 
-        full_stack = Vacancy('Full-Stack', ['PHP', 'MySQL', 'JS'], ['PHP - senior', 'MySql - senior', 'JS -middle'])
-        oleg = Employee('Oleg', 120, 'privet@oleg.com', 'programmer')
+    full_stack = Vacancy('Full-Stack', ['PHP', 'MySQL', 'JS'], ['PHP - senior', 'MySql - senior', 'JS -middle'])
+    oleg = Employee('Oleg', 120, 'privet@oleg.com', 'programmer')
 
-        print(Programmer.comparison_stack(igor, sasha))
-        print(Employee.check_salary(sasha, Employee.day_count))
-        print(sasha.__dict__)
-        print(igor.__dict__)
-        print(Programmer.comparison_stack(sasha, igor))
-        print(Employee.check_salary(igor, Employee.day_count))
+    # print(Programmer.comparison_stack(igor, sasha))
+    # print(Employee.check_salary(sasha, Employee.day_count))
+    # print(sasha.__dict__)
+    # print(igor.__dict__)
+    # print(Programmer.comparison_stack(sasha, igor))
+    # print(Employee.check_salary(igor, Employee.day_count))
 try:
     Candidate.work(john)
+
 except Exception as err:
     with open('logs.txt', 'a') as f:
         message = '{}    {}:\n {}\n \n'.format(datetime.datetime.now(), err.__class__.__name__, traceback.format_exc())
         f.write(message)
+
 try:
     Employee.validate_email(oleg, "privet@oleg.com")
-
 
 except Exception as err:
     with open('logs.txt', 'a') as f:
